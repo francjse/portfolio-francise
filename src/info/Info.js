dbaab3b4499682ba5d52e864d480f47cf6f8cc04
Yujun Liu <yujunliu150@gmail.com>
2024-11-17 16:30:04 -0500

    updated icon and stuff

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
-import "../styles/info.css";
-import { ReactComponent as InfoSVG } from '../assets/info_icon3.svg'; // Import the SVG as a React component
-import sampleVideo from '../assets/skull.mp4';
+import '../styles/info.css';
 
 export default class Info extends Component {
   render() {
     return (
-      <div id="content">
-        <InfoSVG className="about-svg" />
-        <video 
-          className="video-overlay"
-          src={sampleVideo} 
-          autoPlay 
-          loop 
-          muted 
-        />
-      </div>
-    );
+      <div id='content'>Works</div>
+    )
   }
 }
